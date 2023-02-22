bae1520b039232d358a0c3d898e7c6655c25b9b9
arafathsiam99 <md.arafathislamsiamnstuam14@gmail.com>
2023-02-23 02:38:09 +0600

    first commit

---
+++ routes/users.route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  AddUser,
+  FindAllUsers,
+  FindSingUser,
+  UpdateUser,
+  DeleteUser,
+} = require("../controllers/users.controller");
+const router = express.Router();
+
+// add user
+router.post("/users", AddUser);
+// find all users
+router.get("/users", FindAllUsers);
+// find single user
+router.get("/users/:id", FindSingUser);
+// update user
+router.put("/users/:id", UpdateUser);
+// delete user
+router.delete("/users/:id", DeleteUser);
+
+module.exports = router;
